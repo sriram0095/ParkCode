55443fbe7d090a67b8c2b287dd90af149a689f07
sriram0095 <govindusriram10@gmail.com>
2026-04-24 23:38:52 +0530

    Added expiry logic and nearby filtering

--- server/server.js
+++ server/server.js
@@ -21,7 +21,29 @@ app.get("/", (req, res) => {
 // 👇 EXISTING route
 app.get("/api/parking", async (req, res) => {
   try {
-    const spots = await ParkingSpot.find();
+    const { lat, lng } = req.query;//user tells his loc to fetch nearby spots 
+
+    const expiryTime = 30 * 60 * 1000;//after 30mins the spot get expired
+
+    let spots = await ParkingSpot.find({// fetches data from MongoDB
+      updatedAt: { $gte: new Date(Date.now() - expiryTime) }
+    });
+
+    // If location is provided → filter nearby
+    if (lat && lng) {
+      const userLat = parseFloat(lat);
+      const userLng = parseFloat(lng);
+
+      spots = spots.filter(spot => {
+        const distance = Math.sqrt(//here we use basic formula d=sqrt(x2-x1)^2+(y2-y1)^2
+          Math.pow(spot.location.lat - userLat, 2) +
+          Math.pow(spot.location.lng - userLng, 2)
+        );
+        // filters spots within a small radius (~1km approx)
+        return distance < 0.01; // adjust this radius
+      });
+    }
+
     res.json(spots);
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -29,10 +51,23 @@ app.get("/api/parking", async (req, res) => {
 });
 
 
-// 👇 ADD YOUR POST API HERE (IMPORTANT)
-app.post("/api/parking", async (req, res) => {
+app.put("/api/parking/:id", async (req, res) => {
   try {
-    const spot = new ParkingSpot(req.body);
+    const updatedSpot = await ParkingSpot.findByIdAndUpdate(//used to update few details in existing data
+      req.params.id,//we update the data using its id
+      req.body,
+      { new: true }
+    );
+    res.json(updatedSpot);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// 👇 ADD YOUR POST API HERE (IMPORTANT)
+app.post("/api/parking", async (req, res) => {//to create the data like loc,pluscode,status,vehicletype etc
+  try {
+    const spot = new ParkingSpot(req.body);//ParkingSpot is a Mongoose model (like a class for MongoDB collection)
     await spot.save();
     res.json(spot);
   } catch (err) {
@@ -45,3 +80,22 @@ app.post("/api/parking", async (req, res) => {
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+
+
+
+
+
+/*
+GET	|Read data|	View parking
+POST	|Create data|	Add parking
+PUT	|Update data|	Mark full*/
+
+/*req → request (data coming in)
+res → response (data you send back)
+req.body
+Data sent by user (POST / PUT)
+req.query
+?lat=17.385&lng=78.4867
+ Query parameters (used for filtering)
+*/
